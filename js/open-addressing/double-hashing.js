8a0856902adad1a10bc0aa65df487e960091749e
deongracias <deongracias1@gmail.com>
2022-02-16 14:14:32 +0530

    lenght of hashmap can be given

--- js/open-addressing/double-hashing.js
+++ js/open-addressing/double-hashing.js
@@ -1,63 +1,76 @@
 function drawHashMap(hashes) {
-    return `<div style="border:1px solid black;width:max-content;display:grid;grid-template-columns: 1fr 1fr;">${Object.keys(hashes).map(hash => {
-        return `<div>${hash}</div><div>${hashes[hash]}</div>`
-    }).join("")}</div>`
+  return `<div style="border:1px solid black;width:max-content;display:grid;grid-template-columns: 1fr 1fr;">${Object.keys(
+    hashes
+  )
+    .map((hash) => {
+      return `<div>${hash}</div><div>${hashes[hash]}</div>`;
+    })
+    .join("")}</div>`;
 }
 
-function oldDoubleHashing(elements) {
-    let n = elements.length
+function oldDoubleHashing(elements, len) {
+  let n = len ? len : elements.length;
 
-    let hashes = {};
+  let hashes = {};
 
-    let result = elements.map(element => {
-        let res = '<div style="display: grid; grid-template-columns: min-content max-content; grid-column-gap: 1rem">';
+  let result = elements.map((element) => {
+    let res =
+      '<div style="display: grid; grid-template-columns: min-content max-content; grid-column-gap: 1rem">';
 
-        let i = 0;
+    let i = 0;
 
-        let hash = (element + i * (element % (n - 1))) % n;
-        res += `<div>H(${element})</div><div> = ${element} + ${i} * (${element} % ${n - 1}) % ${n} = ${hash}</div>`
+    let hash = (element + i * (1 + (element % (n - 1)))) % 7;
+    res += `<div>H(${element})</div><div> = [${element} + ${i} * (1 + ${element} % ${
+      n - 1
+    })] % ${n} = ${hash}</div>`;
 
-        while (hash in hashes && i < 4) {
-            i++;
-            hash = (element + i * (element % (n - 1))) % n;
-            res += `<div></div><div> = ${element} + ${i} * (${element} % ${n - 1}) % ${n} = ${hash}</div>`
-            console.log(i)
-        }
-        hashes[hash] = element;
-        return `${res}</div><div>${drawHashMap(hashes)}</div>`;
-    })
+    while (hash in hashes && i < 4) {
+      i++;
+      hash = (element + i * (1 + (element % (n - 1)))) % 7;
+      res += `<div></div><div> = [${element} + ${i} * (1 + ${element} % ${
+        n - 1
+      })] % ${n} = ${hash}</div>`;
+    }
 
-    document.getElementById("result").innerHTML = `
+    hashes[hash] = element;
+    return `${res}</div><div>${drawHashMap(hashes)}</div>`;
+  });
+
+  document.getElementById("result").innerHTML = `
     <div style="display: grid; grid-template-columns: max-content max-content; gap: 1rem">
-        ${result.map(e => `${e}`).join("")}
-    </div>`
+        ${result.map((e) => `${e}`).join("")}
+    </div>`;
+}
+function doubleHashing(elements, len) {
+  let n = len ? len : elements.length;
 
-} function doubleHashing(elements) {
-    let n = elements.length
+  let hashes = {};
 
-    let hashes = {};
+  let result = elements.map((element) => {
+    let res =
+      '<div style="display: grid; grid-template-columns: min-content max-content; grid-column-gap: 1rem">';
 
-    let result = elements.map(element => {
-        let res = '<div style="display: grid; grid-template-columns: min-content max-content; grid-column-gap: 1rem">';
+    let i = 0;
 
-        let i = 0;
+    let hash = (element + i * (1 + (element % (n - 1)))) % 7;
+    res += `<div>H(${element})</div><div> = [${element} + ${i} * (1 + ${element} % ${
+      n - 1
+    })] % ${n} = ${hash}</div>`;
 
-        let hash = (element + i * (1 + (element % (n - 1)))) % 7;
-        res += `<div>H(${element})</div><div> = [${element} + ${i} * (1 + ${element} % ${n - 1})] % ${n} = ${hash}</div>`
+    while (hash in hashes && i < 4) {
+      i++;
+      hash = (element + i * (1 + (element % (n - 1)))) % 7;
+      res += `<div></div><div> = [${element} + ${i} * (1 + ${element} % ${
+        n - 1
+      })] % ${n} = ${hash}</div>`;
+    }
 
-        while (hash in hashes && i < 4) {
-            i++;
-            hash = (element + i * (1 + (element % (n - 1)))) % 7;
-            res += `<div></div><div> = [${element} + ${i} * (1 + ${element} % ${n - 1})] % ${n} = ${hash}</div>`
-            console.log(i)
-        }
+    hashes[hash] = element;
+    return `${res}</div><div>${drawHashMap(hashes)}</div>`;
+  });
 
-        hashes[hash] = element;
-        return `${res}</div><div>${drawHashMap(hashes)}</div>`;
-    })
-
-    document.getElementById("result").innerHTML = `
+  document.getElementById("result").innerHTML = `
     <div style="display: grid; grid-template-columns: max-content max-content; gap: 1rem">
-        ${result.map(e => `${e}`).join("")}
-    </div>`
+        ${result.map((e) => `${e}`).join("")}
+    </div>`;
 }
--- js/open-addressing/quadratic-probe.js
+++ js/open-addressing/quadratic-probe.js
@@ -1,57 +1,67 @@
 function drawHashMap(hashes) {
-    return `<div style="border:1px solid black;width:max-content;display:grid;grid-template-columns: 1fr 1fr;">${Object.keys(hashes).map(hash => {
-        return `<div>${hash}</div><div>${hashes[hash]}</div>`
-    }).join("")}</div>`
+  return `<div style="border:1px solid black;width:max-content;display:grid;grid-template-columns: 1fr 1fr;">${Object.keys(
+    hashes
+  )
+    .map((hash) => {
+      return `<div>${hash}</div><div>${hashes[hash]}</div>`;
+    })
+    .join("")}</div>`;
 }
 
-function oldQuadraticProbe(elements) {
-    let n = elements.length
+function oldQuadraticProbe(elements, len) {
+  let n = len ? len : elements.length;
 
-    let hashes = {};
+  let hashes = {};
 
-    let result = elements.map(element => {
-        let res = '<div style="display: grid; grid-template-columns: max-content max-content; grid-column-gap: 1rem">';
-        let i = 0, c1 = 1, c2 = 3;
-        let hash = (element + c1 * i + c2 * (i ** 2)) % n;
-        res += `<div>H(${element})</div><div> = {${element} + (${c1} * ${i}) + [${c2} * (${i} ^ 2)]} % ${n} = ${hash}</div>`;
+  let result = elements.map((element) => {
+    let res =
+      '<div style="display: grid; grid-template-columns: max-content max-content; grid-column-gap: 1rem">';
+    let i = 0,
+      c1 = 1,
+      c2 = 3;
+    let hash = (element + c1 * i + c2 * i ** 2) % n;
+    res += `<div>H(${element})</div><div> = {${element} + (${c1} * ${i}) + [${c2} * (${i} ^ 2)]} % ${n} = ${hash}</div>`;
 
-        while (hash in hashes && i < 4) {
-            i++;
-            hash = (element + c1 * i + c2 * (i**2)) % n;
-            res += `<div></div><div> = {${element} + (${c1} * ${i}) + [${c2} * (${i} ^ 2)]} % ${n} = ${hash}</div>`
-        }
-        hashes[hash] = element;
-        return `${res}</div><div>${drawHashMap(hashes)}</div>`;
-    })
+    while (hash in hashes && i < 4) {
+      i++;
+      hash = (element + c1 * i + c2 * i ** 2) % n;
+      res += `<div></div><div> = {${element} + (${c1} * ${i}) + [${c2} * (${i} ^ 2)]} % ${n} = ${hash}</div>`;
+    }
+    hashes[hash] = element;
+    return `${res}</div><div>${drawHashMap(hashes)}</div>`;
+  });
 
-    document.getElementById("result").innerHTML = `
+  document.getElementById("result").innerHTML = `
     <div style="display: grid; grid-template-columns: max-content max-content; gap: 1rem">
-        ${result.map(e => `${e}`).join("")}
-    </div>`
+        ${result.map((e) => `${e}`).join("")}
+    </div>`;
 }
 
-function quadraticProbe(elements) {
-    let n = elements.length
+function quadraticProbe(elements, len) {
+  let n = len ? len : elements.length;
 
-    let hashes = {};
+  let hashes = {};
 
-    let result = elements.map(element => {
-        let res = '<div style="display: grid; grid-template-columns: max-content max-content; grid-column-gap: 1rem">';
-        let i = 0, c1 = 1, c2 = 3;
-        let hash = (element + i * i) % n;
-        res += `<div>H(${element})</div><div> = {${element} + (${i} * ${i}) % ${n} = ${hash}</div>`;
+  let result = elements.map((element) => {
+    let res =
+      '<div style="display: grid; grid-template-columns: max-content max-content; grid-column-gap: 1rem">';
+    let i = 0,
+      c1 = 1,
+      c2 = 3;
+    let hash = (element + i * i) % n;
+    res += `<div>H(${element})</div><div> = {${element} + (${i} * ${i}) % ${n} = ${hash}</div>`;
 
-        while (hash in hashes) {
-            i++;
-            hash = (element + i * i) % n;
-            res += `<div>H(${element})</div><div> = {${element} + (${i} * ${i}) % ${n} = ${hash}</div>`;
-        }
-        hashes[hash] = element;
-        return `${res}</div><div>${drawHashMap(hashes)}</div>`;
-    })
+    while (hash in hashes) {
+      i++;
+      hash = (element + i * i) % n;
+      res += `<div>H(${element})</div><div> = {${element} + (${i} * ${i}) % ${n} = ${hash}</div>`;
+    }
+    hashes[hash] = element;
+    return `${res}</div><div>${drawHashMap(hashes)}</div>`;
+  });
 
-    document.getElementById("result").innerHTML = `
+  document.getElementById("result").innerHTML = `
     <div style="display: grid; grid-template-columns: max-content max-content; gap: 1rem">
-        ${result.map(e => `${e}`).join("")}
-    </div>`
+        ${result.map((e) => `${e}`).join("")}
+    </div>`;
 }
